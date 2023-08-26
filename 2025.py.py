cfe99dbf425736a2cf413dcd56d5a1cc664abe0d
João Victor <142749045+thevyct@users.noreply.github.com>
2023-08-25 21:50:43 -0300

    Add files via upload

---
+++ 2025.py.py
@@ -0,0 +1,30 @@
+
+print(12*12.50)
+
+print(2025-2004)
+
+print(2050-2023)
+
+print("Em que ano você nasceu?")
+nascimento=input()
+nascimento=int(nascimento)
+idadeEm2025 = 2025 - nascimento
+print( "Em 2025 você terá", idadeEm2025, "anos!" )
+
+
+
+print("Em que ano você nasceu?")
+nascimento=input()
+nascimento=int(nascimento)
+    
+print("Para qual ano você quer saber sua idade?")
+ano=input()
+ano=int(ano)
+print("Em", ano, "você terá", ano-nascimento, "anos!")
+
+
+
+print("Quantos anos você tem?")
+x=input()
+x=int(x)
+print("Se você fosse um cachorro, você teria", x * 7 , "anos!")
